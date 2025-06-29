9cba2b8f4c4979e04a97f298bfb293ea63c12679
Jacob Kim <56609938+peepulp@users.noreply.github.com>
2025-06-29 19:49:47 -0400

    Extract credentials from original Node.js backend

---
+++ backend-master/mysql/mysql_config.js
@@ -0,0 +1,9 @@
+let config = {
+  host: 'localhost',
+  user: 'root',
+  password: 'your_mysql_password_here',  // UPDATE THIS
+  database: 'wespeakwelisten',
+  port: 3306  // Changed from socket to port
+};
+
+module.exports = config;
